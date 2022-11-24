342da364f036d2e3886b175c8112943b95c98cc5
ArunWarrier123 <arunwarrier14@gmail.com>
2022-11-24 15:48:36 +0530

    movedimgtopublic

--- src/Components/DeveloperHomeDiv.js
+++ src/Components/DeveloperHomeDiv.js
@@ -4,7 +4,7 @@ function DeveloperHomeDiv(props) {
     //let imglink = props.img_link;
     //console.log(imglink);
     return<>
-    <div className='divnum1' style={{backgroundImage: "url(/images/div1background.png)"}}>
+    <div className='divnum1' style={{backgroundImage: "url(/div1background.png)"}}>
         <div className='textarea'>
             <div className='headertext'>{props.text1}<br></br>{props.text2}</div>
             <div className='undertext'>{props.subtext}
